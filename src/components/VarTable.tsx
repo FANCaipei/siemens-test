e9cbc92af257ce4ecd296bdec855aba5d99a8b8b
Caipei <fancaipei@gmail.com>
2026-04-02 10:30:51 +0800

    VarTable component

--- src/components/VarTable.tsx
+++ src/components/VarTable.tsx
@@ -3,15 +3,47 @@ import type { ColumnsType } from 'antd/es/table'
 import { useTableDataStore } from '../store/tableDataStore'
 import type { TableRow } from '../store/tableDataStore'
 
-const columns: ColumnsType<TableRow> = []
+const columns: ColumnsType<TableRow> = [
+  {
+    title: 'Index',
+    dataIndex: 'id',
+    key: 'id',
+    width: 90,
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Data Type',
+    dataIndex: 'dataType',
+    key: 'dataType',
+    width: 140,
+  },
+  {
+    title: 'Default Value',
+    dataIndex: 'defaultValue',
+    key: 'defaultValue',
+    width: 160,
+    render: (v: TableRow['defaultValue']) => String(v),
+  },
+  {
+    title: 'Comment',
+    dataIndex: 'comment',
+    key: 'comment',
+  },
+]
 
 export default function VarTable() {
   const tableData = useTableDataStore((s) => s.tableData)
 
   return (
     <Table
+      rowKey="id"
       columns={columns}
       dataSource={tableData}
+      pagination={false}
     />
   )
 }
